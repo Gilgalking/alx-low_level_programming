6c13b6e9930572d1a88cde58df76c2a467cf6546
Gilgalking <gilgalking@gmail.com>
2023-02-16 19:53:06 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to a standard error
- * Return 1
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ *   followed by a new line, to the standard error.
+ * Return 0 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+wtite(2"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
