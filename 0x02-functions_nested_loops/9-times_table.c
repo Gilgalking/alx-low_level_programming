dbf7db44471e4697d58904b62bb210b4ee5f70a5
Gilgalking <gilgalking@gmail.com>
2023-02-21 09:20:14 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,11 +25,11 @@ _putchar(k + '0');
 }
 {
 else if (k >= 10)
-{
+
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
+_putchar((k % 10) + '0');}
 }
 }
 _putchar('\n');
